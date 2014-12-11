a8e9481cc14216f41669b2598506dec1d64311df
Ryan McMinn <git@ryanmcminn.com>
2014-12-10 20:20:35 -0800

    woo hoo I made a button and have it playing an audio file I whipped up in Garageband

--- Meditate/ViewController.swift
+++ Meditate/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
+
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func playAudio(sender: AnyObject) {
+        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("SimpleAmbient", ofType: "m4a")!)
+        println(alertSound)
+        
+        var error:NSError?
+        audioPlayer = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+       
+    }
+    
+    var audioPlayer = AVAudioPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
